b6ff960fd22f29a139369cc2b9378ad5a00207f7
ebalexander <ealexander@seas.harvard.edu>
2016-03-28 15:00:01 -0400

    comments

--- RMI/src/chatapp/TestClient.java
+++ RMI/src/chatapp/TestClient.java
@@ -5,9 +5,31 @@ import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.HashSet;
 
+/**
+ * test class for chatapp, client 1
+ * <p>
+ * This class has a main method that starts a two ChatClients and tests 
+ * the specified functionality: creates accounts, logs in, lists accounts
+ * (tests wildcard), creates and lists groups, sends and checks group 
+ * messages, logs out, and deletes accounts.
+ */
 public class TestClient {
+	/**
+     	 * hardcoded IP address of the server the client will connect to
+     	 */
 	private static String hostname = "10.252.192.83";
+	
+	/**
+     	 * hardcoded port on the server the client will connect to
+     	 */
 	private static int port = 1358;
+	
+	/**
+     	 * Attempt to start a ChatClient connected to ChatServer
+     	 * at hardcoded IP address and port. Test functionality.
+     	 * 
+     	 * @param args ignored
+     	 */
 	public static void main(String[] args) throws RemoteException {
 		if (args.length > 0) {
 			hostname = args[0];
