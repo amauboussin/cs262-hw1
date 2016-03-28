1c7002b2f40f3b66e64f3935aed74657ad7cc9cc
ebalexander <ealexander@seas.harvard.edu>
2016-03-28 15:35:46 -0400

    comments

--- RMI/src/chatapp/MessageImpl.java
+++ RMI/src/chatapp/MessageImpl.java
@@ -3,14 +3,39 @@ package chatapp;
 import java.io.Serializable;
 
 public class MessageImpl implements Message, Serializable {
-	
+	/**
+	 * version number, because Message is Serializable
+	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * username of sender
+	 */
 	private String fromID;
+	
+	/**
+	 * username of recipient
+	 */
 	private String toID;
+	
+	/**
+	 * text of message
+	 */
 	private String msgStr;
+	
+	/**
+	 * timestamp of message
+	 */
 	private Long msgTime;
 	
+	/**
+	 * Creates a message
+	 * 
+	 * @param fromUser username of sender
+	 * @param toUser   username of recipient
+	 * @param msgText  text of message
+	 * @param msgT     timestamp of message
+	 */
 	public MessageImpl(String fromUser, String toUser, String msgText, Long msgT) {
 		fromID = fromUser;
 		toID = toUser;
