184fe70c97fb686698c30973c05634eeec9b1a62
Andrew Mauboussin <andrew@kensho.com>
2016-03-29 14:01:11 -0400

    added most of docs

--- chatappWire/chat_server.py
+++ chatappWire/chat_server.py
@@ -25,11 +25,16 @@ def login(requester, username):
     response = 'Logged in as %s' % username
     if len(queued_messages[username]):
         response += '\n' + '\n'.join(queued_messages[username])
-        queued_messages[username] = [] #  reset queue
+        queued_messages[username] = []  # reset queue
     return response
 
 
 def logout(requester):
+    """Logout the user on the given socket
+
+    Args:
+        requester(socket.socket): Socket of user to be logged out
+    """
     handle_disconnect(requester)
 
 
@@ -57,6 +62,18 @@ def create_group(requester, name, *members):
 
 
 def message(requester, user, message, from_queue=False):
+    """Send a message to a user if they are online
+
+    Args:
+        requester(socket.socket): Read socket of the account sending the message
+        user(str): Account to send message to
+        message(str): Message to sent
+        from_queue (bool): Whether the message has been previously queued
+            (if it is from the queue it has already been formatted)
+    Returns:
+        String describing the status of the message
+
+    """
     from_user = socket_username[requester]
     if not from_queue:
         message = '%s: %s' % (from_user, message)
@@ -72,30 +89,53 @@ def message(requester, user, message, from_queue=False):
 
 
 def message_group(requester, group, to_send):
+    """Message "to_send" to each user in the given group if it exists"""
     if group in groups:
         for user in groups[group]:
             message(requester, user, to_send)
     else:
-       return 'Group %s does not exist\n' % group
+        return 'Group %s does not exist\n' % group
 
 
 def _filter_names(names, query):
-    '''Filter by the given wildcard query'''
+    """Filter a list of names by the given wildcard query
+    Args:
+        names (str list): list of names to be filtered
+        query(str): string that each name must contain
+    """
     check_match = lambda n: re.match(query.replace('*', '.*'), n)
     return sorted(filter(check_match, names))
 
 
 def list_groups(requester, query='*'):
+    """List all groups that match the given query
+    Args:
+        requester(socket.socket): Socket to send confirmation message to
+        query(str): Optional, string that group names must contain
+
+    """
     group_names = _filter_names(groups.keys(), query)
     return ', '.join(group_names)
 
 
 def list_accounts(requester, query='*'):
+    """List all accounts that match the given query
+    Args:
+        requester(socket.socket): Socket to send confirmation message to
+        query(str): Optional, string that account names must contain
+
+    """
     account_names = _filter_names(list(accounts), query)
     return ', '.join(account_names)
 
 
 def delete_account(requester, to_delete):
+    """Delete a user account
+
+    Args:
+        requester(socket.socket): Socket to send confirmation message to
+        to_delete(str): Username of account to be deleted
+    """
     if to_delete in socket_username.values():
         return 'Account %s is currently logged in' % to_delete
     elif to_delete in accounts:
@@ -107,7 +147,14 @@ def delete_account(requester, to_delete):
 
 
 def send(socket, message):
-    '''Send the given message to the given recipient'''
+    """Send a given message over a socket (complete with a header)
+    Args:
+        socket(socket.socket): socket to send the message over
+        message (str): message to be sent
+
+    If the given socket is closed, disconnects that user
+    """
+
     if not len(message):
         log('Tried to send blank message')
     header = serialize_header(len(message))
@@ -119,7 +166,7 @@ def send(socket, message):
 
 
 def handle_disconnect(socket):
-    '''Logs out the user associated with the given socket'''
+    """Clean up data associated with the user on given socket after their disconnect"""
     all_sockets.remove(socket)
     disconnected_user = socket_username[socket]
     log(disconnected_user)
@@ -128,34 +175,57 @@ def handle_disconnect(socket):
 
 
 def get_command(command):
+    """Try to load the given command from the global namespace
+
+    Args:
+        command(str): Name of the command to try to load
+    Returns:
+        command (function): Function definition of the command
+    Raises:
+        Custom exception if a command is not defined in the global namespace
+    """
     if command in globals():
         return globals()[command]
     else:
         raise Exception('Command %s does not exist' % command)
 
+#  load the function definition for each possible command
 commands = {c: get_command(c) for c in commands}
 
+
 def parse_client_message(requester, message):
-    '''Parse a request from a client and return a response'''
+    """Parse a request from a client and return a response
+
+    Args:
+        requester (socket.socket): socket of the message sender
+        message (str): message contents
+
+    No return value. Sends a message to the client confirming
+    the command was executed if successful, otherwise logs failure.
+    """
     command, args = parse_body(message)
     if command in commands:
         try:
+            #  each command takes the requester as the first argument,
+            #  followed by any additional arguments
             response = commands[command](requester, *args)
         except TypeError as e:
-            response = str(e) 
+            response = str(e)
     else:
         response = 'Command %s does not exist.' % command
-    
+
     if response:
         send(requester, response)
 
+
 def main():
-    
+    """Instantiate the server and begin actively listen for clients"""
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('0.0.0.0', PORT))
     server_socket.listen(MAX_CLIENTS)
- 
+
     all_sockets.append(server_socket)
 
     log('Server started on port %s' % PORT)
@@ -163,6 +233,8 @@ def main():
     while True:
         # Get a list of sockets that send data. does not work on winodws
         read_sockets, _, _ = select.select(all_sockets, [], [])
+
+        #  check for data on the server socket and each of the client sockets
         for s in read_sockets:
 
             #  data from the server is a new connection
--- chatappWire/utils.py
+++ chatappWire/utils.py
@@ -3,8 +3,8 @@ VERSION = '1.00'
 HEADER_SIZE = 16
 MAX_CLIENTS = 10
 
-
-help = {
+#  list of commands and their arguments
+command_args = {
     'login': 'username',
     'logout': 'No Arguments',
     'create_account': 'username',
@@ -16,44 +16,71 @@ help = {
     'delete_account': 'username',
 }
 
-commands = help.keys()
+commands = command_args.keys()
+
 
 def get_help():
+    """Return a string with a list of all available commands and their arguments"""
     s = 'Function: Arguments\n'
-    for f, args in help.items():
+    for f, args in command_args.items():
         s += '%s: %s\n' % (f, args)
     return s
 
+
 def parse_body(message):
-    pieces = map(lambda s:s.strip(), message.split('|'))
+    """Return the function being requested and its arguments given a message string"""
+    pieces = map(lambda s: s.strip(), message.split('|'))
     command = pieces[0].strip()
     args = pieces[1:] if len(pieces) > 1 else ()
     return command, args
 
+
 def parse_header(header):
+    """Return the wire protocol version and payload size given a header string"""
     version, payload_size = header.split('|')
     payload_size = int(float(payload_size))
     return version, payload_size
 
 
 def serialize_header(payload_size):
+    """Create the header for a request
+
+    Args:
+        payload_size (int): Payload size of the request in characters
+    Returns:
+        header (str): Header with current version and payload_size to be sent to server
+    """
     header = '%s|%d.' % (VERSION, payload_size)
     padding_length = HEADER_SIZE - len(header)
     return header + '0' * padding_length
 
+
 def serialize_request(command):
+    """Create a request from a string command using the custom wire protocol
+
+    Args:
+        command (str): A space-delimitedstring with the name of the function
+            being called followed by the arguments (e.g. "message andrew hi")
+    Returns:
+        header (str): Header of the request to be sent to the server
+        body (str): Body of the request to be sent to the server
+
+    If the given command is invalid (not a function or wrong number of arguments,
+    return (None, None)
+    """
+
     if command.find('|') != -1:
         print 'Illegal character |'
         return None, None
 
     pieces = command.split(' ', 1)
     comm_string = pieces[0].strip()
-    args = pieces[1] if len(pieces) > 1 else None  
-    
+    args = pieces[1] if len(pieces) > 1 else None
+
     if comm_string not in commands:
         print 'Invalid command %s. Type "help" to see options.' % comm_string
         return None, None
-    
+
     if comm_string in ('message', 'message_group'):
         if not ' ' in args:
             print 'Not enough arguments'
@@ -74,12 +101,16 @@ def serialize_request(command):
     header = serialize_header(len(body))
     return header, body
 
+
 def run_tests():
+    """Run all utility function tests"""
     test_serialize_request()
 
+
 def test_serialize_request():
-    print serialize_request('message andrew hi') 
-    print serialize_request('create_group my_group andrew alex eric emma dennis') 
+    """Print the result of serializing a couple messages to stdout"""
+    print serialize_request('message andrew hi')
+    print serialize_request('create_group my_group andrew alex eric emma dennis')
 
 if __name__ == '__main__':
     run_tests()
